2f313a28f8a4b5f8eaf5d4e6d911c99893c37798
alumno <alumno@172.16.205.110>
2017-02-03 11:39:21 +0100

    First commit

---
+++ src/main/java/com/fpmislata/clientejson/ClienteCategoriaTest.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.fpmislata.clientejson;
+
+import com.fpmislata.domain.Categoria;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.Type;
+import java.util.List;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+/**
+ *
+ * @author alumno
+ */
+public class ClienteCategoriaTest {
+    public static void main(String[] args) throws IOException {
+        System.out.println("Lista de categorias del sistema");
+        System.out.println("---------------------------");
+        List<Categoria> lista = getListCategoria("http://localhost:8080/ProyectoFinal20162017-web/webservice/CategoriasService/Categorias");
+        for (Categoria categoria : lista) {
+            System.out.println(categoria.toString());
+        }
+        System.out.println("----------------------------\n");
+    }
+    
+    private static List<Categoria> getListCategoria(String url) throws IOException {
+        // Crea el cliente para realizar la conexion
+        DefaultHttpClient httpClient = new DefaultHttpClient();
+        // Crea el método con el que va a realizar la operacion
+        HttpGet httpGet = new HttpGet(url);
+        // Añade las cabeceras al metodo
+        httpGet.addHeader("accept", "application/json; charset=UTF-8");
+        httpGet.addHeader("Content-type", "application/json; charset=UTF-8");
+        // Invocamos el servicio rest y obtenemos la respuesta
+        HttpResponse response = httpClient.execute(httpGet);
+        // Obtenemos un objeto String como respuesta del response
+        String lista = readObject(response);
+        // Creamos el objeto Gson que parseará los objetos a JSON, excluyendo los que no tienen la anotacion @Expose
+        Gson gson = new GsonBuilder().excludeFieldsWithoutExposeAnnotation().create();
+        // Creamos el tipo generico que nos va a permitir devolver la lista a partir del JSON que esta en el String
+        Type type = new TypeToken<List<Categoria>>() {
+        }.getType();
+        // Parseamos el String lista a un objeto con el gson, devolviendo así un objeto List<Categoria>
+        return gson.fromJson(lista, type);
+    }
+    
+    private static String readObject(HttpResponse httpResponse) throws IOException {
+        BufferedReader bufferedReader = null;
+        try {
+            bufferedReader = new BufferedReader(new InputStreamReader(httpResponse.getEntity().getContent(), "UTF-8"));
+            StringBuffer buffer = new StringBuffer();
+            char[] dataLength = new char[1024];
+            int read;
+            while ((read = bufferedReader.read(dataLength)) != -1) {
+                buffer.append(dataLength, 0, read);
+            }
+            System.out.println(buffer.toString());
+            return buffer.toString();
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            if (bufferedReader != null) {
+                bufferedReader.close();
+            }
+        }
+    }  
+}
---
+++ src/main/java/com/fpmislata/domain/Categoria.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.fpmislata.domain;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+import javax.persistence.*;
+import javax.xml.bind.annotation.*;
+
+/**
+ *
+ * @author alumno
+ */
+@Entity
+@Table(name = "categorias")
+@NamedQueries({
+    @NamedQuery(name = "Categoria.findAll", query = "SELECT c FROM Categoria c ORDER BY c.id")
+})
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
+public class Categoria implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_categoria")
+    private int id;
+
+    @Column(nullable = false, length = 50)
+    private String nombre;
+
+    @OneToMany(mappedBy = "categoria", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private Set<Producto> productos;
+    
+    public Categoria() {
+        productos = new HashSet<Producto>();
+    }
+
+    public Categoria(String nombre) {
+        this.nombre = nombre;
+        productos = new HashSet<Producto>();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Set<Producto> getProductos() {
+        return productos;
+    }
+
+    public void setProductos(Set<Producto> productos) {
+        this.productos = productos;
+    }
+
+    @Override
+    public String toString() {
+        return "Categoria{" + "id=" + id + ", nombre=" + nombre + '}';
+    }
+}
